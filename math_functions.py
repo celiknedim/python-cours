a4b60891cd0402629c05bf26b827b496afbdfba3
Nedim Celik <celik.nedim@gmail.com>
2024-02-07 16:56:49 +0100

    added hexadecimal

--- math_functions.py
+++ math_functions.py
@@ -7,6 +7,7 @@ def fib(n):
         return n
     return fib(n-1) + fib(n-2)
 
+######################
 
 def is_prime(N):
     
@@ -21,6 +22,8 @@ def is_prime(N):
         if divise == 0:
             return True
         
+#####################
+        
 # Nombres premiers jumeaux : Identifiez et affichez les paires de nombres 
 # premiers jumeaux (deux nombres premiers dont la différence est de 2) 
 # jusqu'à un certain nombre N ?
@@ -30,3 +33,29 @@ def is_prime(N):
 #     for num in range(2, n):
 #         if is_prime(num) and is_prime(num + 2):
             
+
+######################
+
+
+# Conversion hexadécimale-décimale : 
+# Écrivez un programme qui prend un nombre hexadécimal en entrée
+# (sous forme de chaîne de caractères) et le convertit en décimal    
+
+
+# def hex_to_dec(n):
+#     d = int(n, 16)
+#     return d
+
+
+
+def hex_to_decimal(n):
+    d = 0
+    for digit in n:
+        if '0' <= digit <= '9':
+            d = d * 16 + (ord(digit) - ord('0'))
+        elif 'A' <= digit <= 'F':
+            d = d * 16 + (ord(digit) - ord('A') + 10)
+        else:
+            print("invalid")
+            return None
+    return d
--- test_all.py
+++ test_all.py
@@ -17,3 +17,19 @@ def test_prime():
     assert math_functions.is_prime(100000007) == True
 
 
+def test_hex():
+    assert math_functions.hex_to_dec('A') == 10
+    assert math_functions.hex_to_dec('F') == 15
+    assert math_functions.hex_to_dec('10') == 16
+    assert math_functions.hex_to_dec('2B') == 43
+    assert math_functions.hex_to_dec('B2') == 178
+
+
+
+def test_hex():
+    assert math_functions.hex_to_decimal('A') == 10
+    assert math_functions.hex_to_decimal('F') == 15
+    assert math_functions.hex_to_decimal('10') == 16
+    assert math_functions.hex_to_decimal('2B') == 43
+    assert math_functions.hex_to_decimal('B2') == 178
+    assert math_functions.hex_to_decimal('3A2') == 930
